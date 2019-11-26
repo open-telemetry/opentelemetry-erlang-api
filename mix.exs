06b4ecffc16e473c73a370f429bc5bf2483c5b27
Zach Daniel <zachary.s.daniel@gmail.com>
2019-11-26 00:03:05 -0500

    add .gitignore, address feedback

--- mix.exs
+++ mix.exs
@@ -8,15 +8,27 @@ defmodule OpenTelemetry.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       # We should never have dependencies
-      deps: [],
-      elixirc_paths: ["lib", "src"]
+      deps: deps(),
+      # Docs
+      name: "OpenTelemetry API",
+      # source_url: "https://github.com/USER/PROJECT",
+      # homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+      docs: [
+        main: "OpenTelemetry"
+        # logo: "path/to/logo.png",
+        # extras: ["README.md"]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
+    []
+  end
+
+  def deps() do
     [
-      extra_applications: [:logger]
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
