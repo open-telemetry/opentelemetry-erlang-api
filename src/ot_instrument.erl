26b4963e5a2ce5be373ae8303dc46bae12e06406
Tristan Sloughter <t@crashfast.com>
2020-07-13 08:11:02 -0600

    add some docs to ot_instrument

--- src/ot_instrument.erl
+++ src/ot_instrument.erl
@@ -12,26 +12,34 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% @doc
+%% @doc All measurements are associated with an instrument. To record
+%% measurements for an instrument it must first be created with `new' and
+%% then can be referenced by name.
 %% @end
 %%%-------------------------------------------------------------------------
 -module(ot_instrument).
 
+%% @doc Calls the SDK to create a new instrument which can then be referenced
+%% by name.
+%% @end
 -callback new(opentelemetry:meter(), ot_meter:name()) -> boolean().
 -callback new(opentelemetry:meter(), ot_meter:name(), ot_meter:instrument_opts()) -> boolean().
 
-%% @doc returns an instrument definition which can be used to create a new instrument
+%% @doc Returns an instrument definition which can be used to create a new instrument
 %% by passing to `ot_meter:new_instruments/1'
 %% @end
 -callback definition(ot_meter:name()) -> ot_meter:instrument_definition().
 -callback definition(ot_meter:name(), ot_meter:instrument_opts()) -> ot_meter:instrument_definition().
 
+%% @doc Used by additive instruments to record measurements.
 -callback add(ot_meter:bound_instrument(), number()) -> ok.
 -callback add(opentelemetry:meter(), ot_meter:name(), number(), ot_meter:labels()) -> ok.
 
+%% @doc Used by non-additive instruments to record measurements.
 -callback record(ot_meter:bound_instrument(), number()) -> ok.
 -callback record(opentelemetry:meter(), ot_meter:name(), number(), ot_meter:labels()) -> ok.
 
+%% @doc Returns a measurement tuple that can be based to a batch recording through `ot_meter:batch_record/3'
 -callback measurement(ot_meter:bound_instrument() | ot_meter:name(), number()) ->
     {ot_meter:bound_instrument() | ot_meter:name(), number()}.
 
